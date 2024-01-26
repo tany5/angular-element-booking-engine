815e29b6aec7e88a209e4d4021da76631a6c9922
Tanmaydey2023 <tanmaydey.roldrive@gmail.com>
2024-01-26 13:07:37 +0530

    [FIX] build with webpack and serve

--- scripts/postbuild-bundler.js
+++
@@ -1,9 +0,0 @@
-async function loadModules() {
-    // Import the polyfills and main modules and wait for them to finish
-    await import('./polyfills.js');
-    await import('./main.js');
-
-    // Do any extra stuff you need for init here.
-  }
-
-  loadModules();
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+module.exports = {
+  entry: {
+    "bundle.js": [
+      path.resolve(
+        __dirname,
+        "./dist/booking-web-component/browser/polyfills.js"
+      ),
+      path.resolve(
+        __dirname,
+        "./dist/booking-web-component/browser/styles.css"
+      ),
+      path.resolve(__dirname, "./dist/booking-web-component/browser/main.js"),
+    ],
+  },
+  output: {
+    filename: "[name]",
+    path: path.resolve(__dirname, "dist/booking-web-component/browser/"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
