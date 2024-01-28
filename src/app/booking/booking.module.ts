098bba0e3c3fcbd1259d13e87b297f02d56a8ab0
Tanmaydey2023 <tanmaydey.roldrive@gmail.com>
2024-01-28 10:44:56 +0530

    [FIX] Ui image with lazy load

--- src/app/booking/booking.module.ts
+++ src/app/booking/booking.module.ts
@@ -4,10 +4,11 @@ import { BookingComponent } from './booking.component';
 import { MaterialModule } from '../material/material.module';
 import { SharedModule } from '../shared/shared.module';
 import { BannerComponent } from './UI/banner/banner.component';
+import { NgOptimizedImage } from '@angular/common';
 
 @NgModule({
   declarations: [BookingComponent, BannerComponent],
-  imports: [CommonModule, MaterialModule, SharedModule],
+  imports: [CommonModule, MaterialModule, SharedModule, NgOptimizedImage],
   exports: [BookingComponent],
 })
 export class BookingModule {}
